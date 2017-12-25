71baf28c73525b30dca6a6f5f9b0af402e205c13
spadek67424 <spadek67424@gmail.com>
2017-12-25 23:22:37 +0800

    try b and x1~x5

--- cleanandchange.py
+++ cleanandchange.py
@@ -35,12 +35,30 @@ trynew=pd.DataFrame(newframe)
 
 trynew2=trynew[::-1]
 previousn=50 #50 if from paper, sum the previous 50 n 
-grutenb=list();
+grutenb=list()
+x1=list()
+x2=list()
+x3=list()
+x4=list()
+x5=list()
 for i in range(0,10012):
 	temp=trynew.ix[i:i+49:1,4]
 	meanmag=temp.sum()/previousn
 	grutenb.append(0.434/(meanmag-temp.min())) #0.434 is log e from paper
+
+for x in range(0,len(grutenb)-20):
+	x1.append(grutenb[x]-grutenb[x+4])
+	x2.append(grutenb[x+4]-grutenb[x+8])
+	x3.append(grutenb[x+8]-grutenb[x+12])
+	x4.append(grutenb[x+12]-grutenb[x+16])
+	x5.append(grutenb[x+16]-grutenb[x+20])
 grutenb=pd.DataFrame({'b':grutenb})
-trynew=pd.concat([trynew,grutenb], ignore_index=True, axis=1)
+x1=pd.DataFrame({'x1':x1})
+x2=pd.DataFrame({'x2':x2})
+x3=pd.DataFrame({'x3':x3})
+x4=pd.DataFrame({'x4':x4})
+x5=pd.DataFrame({'x5':x5})
+trynew=pd.concat([trynew,grutenb,x1,x2,x3,x4,x5], ignore_index=False, axis=1)
+
 print(trynew)
 trynew.to_csv('cleanearthquake.csv',encoding='utf-8')
